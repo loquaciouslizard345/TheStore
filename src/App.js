1ee02d76cd2c65adef23bd22b29062fea1a617a3
qmarleyc15 <77942213+qmarleyc15@users.noreply.github.com>
2023-01-29 23:58:06 -0500

    finaldev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ToyObj from './components/ToyObj';
+import StoreData from "./assets/store-data.json";
+import StoreDataSorted from "./assets/store-data-sorted.json";
 
+
+StoreData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
 function App() {
+  const [wishlistItems, setWishlistItems ] = useState([])
+  function addToWishlist(item){
+    setWishlistItems([...wishlistItems, item])
+
+  }
+  function removeFromWishlist(item){
+
+    setWishlistItems( wishlistItems.filter((x) => x !== item));
+
+
+  }
+  
+
+  
+  function calculateWishlistTotal(){
+    let total= 0
+    for (let i=0; i<wishlistItems.length; i++){
+      total += wishlistItems[i].price
+      
+
+    }
+    return total;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to THE Toy Store</h1>
+        <h3>Wishlist Total : {calculateWishlistTotal()}</h3>
+        <button >Sort Prices Low to High</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {StoreDataSorted.map((item, index) => ( 
+        <ToyObj item = {item} addToWishlist = {addToWishlist} removeFromWishlist = {removeFromWishlist}></ToyObj>
+      ))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
-}
+        }
+               
+
 
 export default App;
